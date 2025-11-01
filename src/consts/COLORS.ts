d213d7f4b71d8158f69ad52894569d2ad98c04ab
abcdefghijklqwq <butrahandisya@gmail.com>
2025-11-01 14:00:22 +0700

    feat: add BREAKPOINTS constants and configure natural colors for bg & text palette

--- src/consts/COLORS.ts
+++ src/consts/COLORS.ts
@@ -1,18 +1,38 @@
 const COMMON_COLORS = {
   '--color-white': 'hsl(0deg 0% 100%)',
-  '--color-dark': 'hsl(230deg 20% 8%)'
+  '--color-dark': 'hsl(230deg 20% 8%)',
+
+  '--shadow-s': `inset 0 1px 2px hsl(0deg 0% 100% / 0.19),
+                 0 1px 2px hsl(0deg 0% 0% / 0.19),      
+                 0 2px 4px hsl(0deg 0% 0% / 0.08)`,
+
+  '--shadow-m': `inset 0 1px 2px hsl(0deg 0% 100% / 0.31),
+                 0 2px 4px hsl(0deg 0% 0% / 0.19),
+                 0 4px 8px hsl(0deg 0% 0% / 0.08)`,
+
+  '--shadow-l': `inset 0 1px 2px hsl(0deg 0% 100% / 0.44),
+                 0 4px 6px hsl(0deg 0% 0% / 0.19),
+                 0 6px 10px hsl(0deg 0% 0% / 0.08)`
 };
 
 export const LIGHT_COLORS = {
-  '--color-text': 'hsl(0deg 0% 5%)',
-  '--color-background': 'hsl(0deg 0% 100%)',
+  '--color-bg-1': 'hsl(0deg 0% 90%)',
+  '--color-bg-2': 'hsl(0deg 0% 95%)',
+  '--color-bg-3': 'hsl(0deg 0% 100%)',
+
+  '--color-text-1': 'hsl(0deg 0% 5%)',
+  '--color-text-2': 'hsl(0deg 0% 30%)',
 
   ...COMMON_COLORS
 };
 
 export const DARK_COLORS = {
-  '--color-text': 'hsl(0deg 0% 100%)',
-  '--color-background': 'hsl(230deg 20% 8%)',
+  '--color-bg-1': 'hsl(0deg 0% 0%)',
+  '--color-bg-2': 'hsl(0deg 0% 5%)',
+  '--color-bg-3': 'hsl(0deg 0% 10%)',
+
+  '--color-text-1': 'hsl(0deg 0% 95%)',
+  '--color-text-2': 'hsl(0deg 0% 70%)',
 
   ...COMMON_COLORS
 };

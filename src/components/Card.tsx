014feddf3128c3094b2c9e8f175997271774116e
abcdefghijklqwq <butrahandisya@gmail.com>
2025-11-02 17:23:15 +0700

    feat: add padding prop to Card component and remove default padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,6 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  padding: ${16 / 16}rem ${24 / 16}rem;
   border-radius: ${8 / 16}rem;
 
   box-shadow: var(--shadow);
@@ -21,19 +20,36 @@ const colorMapper = {
   '3': 'var(--color-bg-3)'
 };
 
+const paddingMapper = {
+  'p-small': `${8 / 16}rem ${8 / 16}rem`,
+  'p-medium': `${12 / 16}rem ${12 / 16}rem`,
+  'p-large': `${16 / 16}rem ${16 / 16}rem`,
+
+  small: `${8 / 16}rem ${12 / 16}rem`,
+  medium: `${12 / 16}rem ${16 / 16}rem`,
+  large: `${16 / 16}rem ${20 / 16}rem`
+};
+
 type CardProps = {
   children: React.ReactNode;
+  padding?: 'p-small' | 'p-medium' | 'p-large' | 'small' | 'medium' | 'large';
   shadow?: 'small' | 'medium' | 'large';
   color?: '1' | '2' | '3';
 };
 
-const Card = ({ children, shadow = 'small', color = '2' }: CardProps) => {
+const Card = ({
+  children,
+  shadow = 'small',
+  color = '2',
+  padding = 'p-medium'
+}: CardProps) => {
   return (
     <Container
       style={
         {
           '--shadow': shadowMapper[shadow],
-          '--color': colorMapper[color]
+          '--color': colorMapper[color],
+          padding: paddingMapper[padding]
         } as React.CSSProperties
       }
     >
